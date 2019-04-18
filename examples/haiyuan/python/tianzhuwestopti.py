70ee65f251634ed52ff2c0fd5f91bedbac829f09
simondaout <simondaout@gmail.com>
2019-04-18 14:48:34 +0100

    update example

--- examples/haiyuan/python/tianzhuwestopti.py
+++ examples/haiyuan/python/tianzhuwestopti.py
@@ -11,17 +11,17 @@ nburn=5000 # numbert of burned iterations to not be influenced by itinial values
 inv=inversion(name='Haiyuan Fault System',
           structures=[
               #######################
-              # Option1: maindecol + ramp : simple ramp-decollement system
+              # Option1: mainfault + ramp : simple ramp-decollement system
               #######################
-              maindecol(
+              mainfault(
               # define one main fault and various optional secondary faults
-              #ss: strike-slip, short: shortening, w: depth
-              name='JQH',D=0.,sigmaD=5,ss=10.,sigmass=10.,short=-5.,
-              sigmashort=5.,w=15.,sigmaw=15,dip=180.,
+              #ss: strike-slip, ds: dip-slip, w: depth, D: horizontal distance to the center of profile
+              name='JQH',D=0.,sigmaD=5,ss=10.,sigmass=10.,ds=-5.,
+              sigmads=5.,w=15.,sigmaw=15,dip=180.,
               # optional: define prior distribution for each parameters (default: 'Unif')
-              distss='Unif',distshort='Unif',distH='Unif'
+              distss='Unif',distds='Unif',distH='Unif'
               ),
-              # D: horizontal distance to the main fault, H: vertical distance to the main fault
+              # D: horizontal distance to the mainfault, H: vertical distance to the mainfault
               ramp(
                 name='QT',ss=0.,sigmass=0.,D=30,sigmaD=25.,H=15.,sigmaH=15
                 ),
@@ -29,17 +29,17 @@ inv=inversion(name='Haiyuan Fault System',
               #######################
               # Option 2: flower structure
               ####################### 
-              # mainflower(
-              #   name='JQH',x=-36.,y=10.2,ss=10.,sigmass=10.,short=-5.,sigmashort=5.,w=15.,sigmaw=15.,dip=180.,
+	      #mainflower(
+              #   name='JQH',ss=10.,sigmass=10.,ds=-5.,sigmads=5.,w=15.,sigmaw=15.,dip=180.,
               #   name2='QT',ss2=0.,sigmass2=0.,D2=30,sigmaD2=25.,H2=15.,sigmaH2=15,
               #   name3='Creeping',ss3=10.,sigmass3=10.,D3=0,sigmaD3=0.,H3=15.,sigmaH3=15,
-              #   distss='Unif',distshort='Unif',distH='Unif'
               # ),
                 ],
           # azimuth of the 2d model: azimuth of the main fault
           strike=-68,
           # l: lenght, w: width, proj: Envisat proj los to east,north,z coordinates (for a mean incidence of 20.5)
-          profile=profile(name='West',x=-36.,y=10.2,l=300,w=50,proj=[0.318, -0.0827, 0.9396]), 
+          profile=profile(name='West',x=-36.,y=10.2,l=300,w=50,proj=[0.318, -0.0827, 0.9396]),
+	  fullcov=False, # set True to compute full covariance matrix
           )
 
 # GPS data (defined in networkopti.py)              
@@ -75,15 +75,4 @@ plotdata=[
 topodata = [
   topo(name='SRTM3',wdir=maindir+'gmt/',filename='topo_km.xy-z',color='black')
 ]
-
-# Parameters covariance  (defined in networkopti.py) 
-fullcov='no' # if no: diagonal only --> try this first
-             # if yes: create full cov from [sudhaus&jonhson09] 
-# optional mask 
-# maskcov = [-10,-10,20,20]
-# param=[None,None,None] # if None: compute [sill, sigm, lamb], if not: go to inversion
-# rampcov='lin' # lin/quad/cub optional ramp
-
-#los coordinates for figure (deprecated?)
-x1,y1=50,-50
 
