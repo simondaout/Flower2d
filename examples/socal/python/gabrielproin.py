da5dcadb5a029a5cb91cf4b8ad738d80664b2f49
simondaout <simondaout@gmail.com>
2019-05-04 12:07:56 +0100

    update example

--- examples/socal/python/gabrielproin.py
+++ examples/socal/python/gabrielproin.py
@@ -9,24 +9,15 @@ ymin,ymax=0,250
 # std: plot mean and standard deviation insar
 # distscale: scatter plot with color scale fct of the profile-parallel distance 
 profiles=[
-        # prof(name='Big Bend',x=-200,y=137,l=200,w=100,strike=-62.5,type='distscale'),
-        # prof(name='Big Bend',x=-200,y=137,l=200,w=100,strike=-62.5,type='std'),
-        prof(name='Big Bend',x=-230,y=160,l=200,w=20,strike=-62.5),
-        prof(name='Big Bend',x=-196,y=144,l=200,w=20,strike=-62.5),
-        prof(name='Big Bend',x=-160,y=125,l=200,w=20,strike=-62.5),
+        profile(name='Big Bend',x=-200,y=137,l=200,w=100,strike=-62.5,type='std',lbins=0.5,flat='quad',loc_ramp='positive'),
+        #profile(name='Big Bend2',x=-196,y=144,l=200,w=20,strike=-62.5),
+        #profile(name='Big Bend3',x=-160,y=125,l=200,w=20,strike=-62.5),
         ]
 
 # InSAR data (defined in networkopti.py)
 insardata=[
-        network(network='t170_cmyr_km.xylos',reduction='T170',wdir=maindir+'insar/',dim=1,scale=-10.,color='red',samp=2)
+        network(network='t170_cmyr_km.xylos',reduction='T170',wdir=maindir+'insar/',dim=1,scale=-10.,color='blue',samp=4)
         ]
-
-# Plot GPS data not implemented yet: not co
-gpsdata=[
-#         network(network='cgps_stations_km.dat',reduction='PBO',wdir=maindir+'gps/',dim=3,scale=1.),
-#         #network(network='cgps_stations_km.dat',reduction='PBO',wdir=maindir+'gps/',dim=3,weight=1.,scale=1.,\
-#         # errorfile='../../output/gps/PBOresidus_0.psvelo'),
-         ]
 
 # topographic file
 topodata=[
