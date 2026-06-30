cbd44e7333efd846872b5a3dc14a34fdf2a0c8bc
Simon Daout <simondaout@gmail.com>
2026-06-30 18:22:00 +0200

    Fix NaN bugs in optimize.py: traceD/traceF/traceH array vs scalar

--- src/flower2d/optimize.py
+++ src/flower2d/optimize.py
@@ -509,7 +509,7 @@ if '{} D'.format(inv.fmodel[0].name) in inv.Sampled:
     m = _get_trace('{} D'.format(inv.fmodel[0].name))
     mf.append(np.mean(m))
     inv.fmodel[0].D = np.mean(m)
-    inv.fmodel[0].traceD = np.mean(m)
+    inv.fmodel[0].traceD = np.asarray(m)
     inv.fmodel[0].sigmaD = 2 * np.std(m)
     inv.traces.append(m)
     inv.labels.append('{} D'.format(inv.fmodel[0].name))
@@ -561,7 +561,7 @@ for j in range(1, inv.Mseg):
         inv.fmodel[j].traceD = np.asarray(m)
         inv.traces.append(m)
         inv.labels.append('{} D'.format(inv.fmodel[j].name))
-        fid = open(inv.outdir + 'stat/{}_L.txt'.format(inv.fmodel[j].name), 'w')
+        fid = open(inv.outdir + 'stat/{}_D.txt'.format(inv.fmodel[j].name), 'w')
         np.savetxt(fid, m)
         fid.close()
 
@@ -584,24 +584,24 @@ for j in range(0, inv.Mseg):
         inv.fmodel[0].sst = inv.fmodel[0].sst + inv.fmodel[j].ss
 
 logger.debug('Saving all plausible models for plot')
-inv.fmodel[0].traceF = inv.fmodel[0].D * np.ones((inv.nsample))
+inv.fmodel[0].traceF = inv.fmodel[0].traceD
 if inv.structures[0].Mseg > 1:
     inv.fmodel[1].tracew = inv.fmodel[0].tracew - inv.fmodel[1].traceH
     inv.fmodel[2].tracew = inv.fmodel[0].tracew - inv.fmodel[2].traceH
-    inv.fmodel[1].traceF = inv.fmodel[1].traceD
-    inv.fmodel[2].traceF = inv.fmodel[2].traceD
+    inv.fmodel[1].traceF = inv.fmodel[0].traceF + inv.fmodel[1].traceD
+    inv.fmodel[2].traceF = inv.fmodel[0].traceF + inv.fmodel[2].traceD
 Mtemp = inv.structures[0].Mseg
 for j in range(1, inv.Mstruc):
     for k in range(inv.structures[j].Mseg):
         if inv.fmodel[Mtemp + k].type == "creep":
             inv.fmodel[Mtemp + k].tracew = np.zeros((inv.nsample))
-            inv.fmodel[Mtemp + k].traceF = inv.fmodel[0].traceF + inv.fmodel[Mtemp + k].D * np.ones((inv.nsample))
+            inv.fmodel[Mtemp + k].traceF = inv.fmodel[0].traceF + inv.fmodel[Mtemp + k].traceD
         elif inv.fmodel[Mtemp + k].type == "vertical":
-            inv.fmodel[Mtemp + k].tracew = inv.fmodel[0].tracew - inv.fmodel[Mtemp + k].H
-            inv.fmodel[Mtemp + k].traceF = inv.fmodel[0].traceF + inv.fmodel[Mtemp + k].D * np.ones((inv.nsample))
+            inv.fmodel[Mtemp + k].tracew = inv.fmodel[0].tracew - inv.fmodel[Mtemp + k].traceH
+            inv.fmodel[Mtemp + k].traceF = inv.fmodel[0].traceF + inv.fmodel[Mtemp + k].traceD
         else:
-            inv.fmodel[Mtemp + k].tracew = inv.fmodel[Mtemp - 1].tracew - inv.fmodel[Mtemp + k].H
-            inv.fmodel[Mtemp + k].traceF = inv.fmodel[Mtemp - 1].traceF + inv.fmodel[Mtemp + k].D
+            inv.fmodel[Mtemp + k].tracew = inv.fmodel[Mtemp - 1].tracew - inv.fmodel[Mtemp + k].traceH
+            inv.fmodel[Mtemp + k].traceF = inv.fmodel[Mtemp - 1].traceF + inv.fmodel[Mtemp + k].traceD
     Mtemp += inv.structures[j].Mseg
 
 for j in range(0, inv.Mvol):
